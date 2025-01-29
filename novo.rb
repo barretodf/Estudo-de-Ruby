e4b5418231f32083783a9d52dc7a58fbba985603
Edmilson Barreto <edsb1981@gmail.com>
2025-01-29 01:59:28 -0300

    Atualizando arquivos e submódulos

--- novo.rb
+++ novo.rb
@@ -4,12 +4,21 @@ class Produtos
         @preco = preco
         @quantidade = quantidade
     end
-    def calcular
-        @preco * @quantidade     
-    end 
-    def mensagem
-        "Para o produto #{@nome}, Temos o valor total de: #{calcular}"
+
+    def soma
+        @preco * @quantidade
     end
 end
-produtos = Produtos.new("Notebook", 2532, 4)
-puts produtos.mensagem
+
+puts "Digite o nome do produto:"
+nome = gets.chomp
+
+puts "Digite o preço do produto"
+preco = gets.chomp.to_f
+
+puts "Digite a quantidade:"
+quantidade = gets.chomp.to_i
+
+produtos = Produtos.new(nome, preco, quantidade)
+
+puts "Para o produto #{produtos.instance_variable_get(:@nome)}, temos R$#{produtos.soma} de valor total."
